191a218e210f7497f1aec748e49e4a37ce571daa
TristanMacaulay <tristanmacaulay6@gmail.com>
2021-03-06 20:00:24 +1100

    created vr 3d enviroment prototype

---
+++ Assets/ActionController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ActionController : MonoBehaviour
+{
+    public GameObject character;
+    private ActionBasedController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>();
+
+        
+    }
+
+    private void ActionPerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        character.GetComponent<CharacterMovement>().Jump();
+    }
+
+    private void MovementPerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Vector2 vec2 = controller.translateAnchorAction.action.ReadValue<Vector2>();
+        Vector3 vec3 = transform.TransformDirection(new Vector3(vec2.x, 0.0f, vec2.y));
+        vec2.x = vec3.x;
+        vec2.y = vec3.z;
+        character.GetComponent<CharacterMovement>().MoveCharacter(vec2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        controller.translateAnchorAction.action.performed += MovementPerformed;
+
+        controller.activateAction.action.performed += ActionPerformed;
+    }
+}
---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //public Vector3 dir;
+    public float speed;
+    public float jumpForce;
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void MoveCharacter(Vector2 dir)
+    {
+        Vector3 actualDir = new Vector3(dir.x, rigidBody.velocity.y, dir.y);
+        FacePosition(dir);
+
+        //transform.Translate(actualDir * speed * Time.deltaTime, Space.World);
+        rigidBody.velocity = actualDir * speed * Time.fixedDeltaTime;
+    }
+
+    public void FacePosition(Vector2 toFace)
+    {
+        Vector3 newPos = new Vector3(toFace.x, transform.position.y, toFace.y);
+        Vector3 currentPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 facePos = currentPos + newPos;
+        facePos.y = transform.position.y;
+        transform.LookAt(facePos);
+    }
+
+    //Jump is called by action controller script
+    public void Jump()
+    {
+        rigidBody.AddForce(new Vector3(rigidBody.velocity.x, jumpForce, rigidBody.velocity.z) * Time.fixedDeltaTime, ForceMode.Impulse);
+    }
+}
